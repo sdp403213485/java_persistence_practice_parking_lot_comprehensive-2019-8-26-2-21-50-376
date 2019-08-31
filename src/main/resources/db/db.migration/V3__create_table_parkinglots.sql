dfc1bceee1b31174fa421eecc2e8392f82db9d88
sdp <403213485@qq.com>
2019-08-31 17:05:06 +0800

    flyway

---
+++ src/main/resources/db/db.migration/V3__create_table_parkinglots.sql
@@ -0,0 +1,6 @@
+CREATE TABLE parkingLots(
+  parkingLotID  VARCHAR PRIMARY KEY,
+  capacity INTEGER,
+  availablePositionCount INTEGER,
+  employeeID  VARCHAR
+);
---
+++ src/main/resources/db/db.migration/V4__add_parkinglots.sql
@@ -0,0 +1,3 @@
+INSERT INTO parkingLots values ('lot_1','100','50','parkingboy_1');
+INSERT INTO parkingLots values ('lot_2','100','60','parkingboy_2');
+INSERT INTO parkingLots values ('lot_3','100','70','parkingboy_3');
